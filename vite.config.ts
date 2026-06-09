e5fed38bd906186bd8148fb5b4a036fec96013cb
dadigua <0laopo0@gmail.com>
2026-06-09 19:10:22 +0800

    feat(vite.config): 为API代理启用WebSocket支持

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,10 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/api": "http://127.0.0.1:8788"
+      "/api": {
+        target: "http://127.0.0.1:8788",
+        ws: true
+      }
     }
   }
 });
